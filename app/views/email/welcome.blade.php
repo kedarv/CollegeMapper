92d631f13d3019fd3850e216d37dac395e1e3749
Kedar <kedar54@gmail.com>
2015-03-17 12:49:00 -0500

    completed email sending
    gmail strips away <head></head> tags..everything must be inlined :(

--- app/views/email/welcome.blade.php
+++ app/views/email/welcome.blade.php
@@ -1,69 +1,44 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/xhtml" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;">
+  <head>
 <!-- If you delete this meta tag, Half Life 3 will never be released. -->
-<meta name="viewport" content="width=device-width" />
-
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>CollegeMapper  |  Welcome</title>
-{{ HTML::style('css/email.css')}}
-</head>
- 
-<body bgcolor="#FFFFFF">
-
-<!-- BODY -->
-<table class="body-wrap">
-	<tr>
-		<td></td>
-		<td class="container" bgcolor="#FFFFFF">
-
-			<div class="content">
-			<table>
-				<tr>
-					<td>
-						<h3>Hey, {{$data['firstname']}}!</h3>
-						<p class="lead">Thanks for using CollegeMapper.</p>
-						<p>As many of you know, a similar app was built for the Class of 2014. While the two websites may <i>look</i> similar, they are completely different from the backend. This version is newer, more robust and mature than 2014Maps. This may mean that there are bugs in the system. If you find any, please contact me ASAP!</p>
-						<!-- Callout Panel -->
-						<p class="callout">
-							I hope you all enjoy the last few weeks of your senior year in high school. The Class of 2014 has left some advice for your class, and you can view it by clicking the link below.
-							<br/>
-							<a href="#">View Page &raquo;</a>
-						</p><!-- /Callout Panel -->	
-						<!-- social & contact -->
-						<table class="social" width="100%">
-							<tr>
-								<td>
-									
-									<!-- column 2 -->
-									<table align="left" class="column">
-										<tr>
-											<td>								
-												<h5 class="">Questions or comments?</h5>												
-                Email: <strong><a href="emailto:vaidyak@purdue.edu">vaidyak@purdue.edu</a></strong></p>
-                
-											</td>
-										</tr>
-									</table><!-- /column 2 -->
-									
-									<span class="clear"></span>	
-									
-								</td>
-							</tr>
-						</table><!-- /social & contact -->
-						<br/>
-						<hr/>
-						<a href="http://github.com/kedarv">@kedarv</a>		
-					</td>
-				</tr>
-			</table>
-			</div><!-- /content -->
-									
-		</td>
-		<td></td>
-	</tr>
-</table><!-- /BODY -->
-
-
-</body>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>CollegeMapper  |  Welcome</title>
+  </head>
+  <body bgcolor="#FFFFFF" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; margin: 0; padding: 0;">&#13;
+&#13;
+<!-- BODY -->&#13;
+<table class="body-wrap" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; width: 100%; margin: 0; padding: 0;"><tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;"><td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;"></td>&#13;
+		<td class="container" bgcolor="#FFFFFF" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto; padding: 0;">&#13;
+&#13;
+			<div class="content" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; max-width: 600px; display: block; margin: 0 auto; padding: 15px;">&#13;
+			<table style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; width: 100%; margin: 0; padding: 0;"><tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;"><td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;">&#13;
+						<h3 style="font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; line-height: 1.1; color: #000; font-weight: 500; font-size: 27px; margin: 0 0 15px; padding: 0;">Hey, {{$data['firstname']}}!</h3>&#13;
+						<p class="lead" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-weight: normal; font-size: 17px; line-height: 1.6; margin: 0 0 10px; padding: 0;">Thanks for using CollegeMapper.</p>&#13;
+						<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-weight: normal; font-size: 14px; line-height: 1.6; margin: 0 0 10px; padding: 0;">As many of you know, a similar app was built for the Class of 2014. While the two websites may <i style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;">look</i> similar, they are completely different from the backend. This version is newer, more robust and mature than 2014Maps. This may mean that there are bugs in the system. If you find any, please contact me ASAP!</p>&#13;
+						<!-- Callout Panel -->&#13;
+						<p class="callout" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-weight: normal; font-size: 14px; line-height: 1.6; background-color: #ECF8FF; margin: 0 0 15px; padding: 15px;">&#13;
+							I hope you all enjoy the last few weeks of your senior year in high school. The Class of 2014 has left some advice for your class, and you can view it by clicking the link below.&#13;
+							<br style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;" /><a href="#" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; color: #2BA6CB; font-weight: bold; margin: 0; padding: 0;">View Page »</a>&#13;
+						</p><!-- /Callout Panel -->	&#13;
+						<!-- social & contact -->&#13;
+						<table class="social" width="100%" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; width: 100%; background-color: #ebebeb; margin: 0; padding: 0;" bgcolor="#ebebeb"><tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;"><td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;">&#13;
+									&#13;
+									<!-- column 2 -->&#13;
+									<table align="left" class="column" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; width: 280px; float: left; min-width: 279px; margin: 0; padding: 0;"><tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;"><td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 15px;">								&#13;
+												<h5 class="" style="font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; line-height: 1.1; color: #000; font-weight: 900; font-size: 17px; margin: 0 0 15px; padding: 0;">Questions or comments?</h5>												&#13;
+                Email: <strong style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;"><a href="emailto:vaidyak@purdue.edu" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; color: #2BA6CB; margin: 0; padding: 0;">vaidyak@purdue.edu</a></strong>&#13;
+                &#13;
+											</td>&#13;
+										</tr></table><!-- /column 2 --><span class="clear" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; display: block; clear: both; margin: 0; padding: 0;"></span>	&#13;
+									&#13;
+								</td>&#13;
+							</tr></table><!-- /social & contact --><br style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;" /><hr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;" /><a href="http://github.com/kedarv" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; color: #2BA6CB; margin: 0; padding: 0;">@kedarv</a>		&#13;
+					</td>&#13;
+				</tr></table></div><!-- /content -->&#13;
+									&#13;
+		</td>&#13;
+		<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin: 0; padding: 0;"></td>&#13;
+	</tr></table><!-- /BODY --></body>
 </html>
