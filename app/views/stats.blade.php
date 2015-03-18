23f6a140b98f6f3138e37ecebce9500610cf2db3
Kedar <kedar54@gmail.com>
2015-03-18 10:24:35 -0500

    added table sorter

--- app/views/stats.blade.php
+++ app/views/stats.blade.php
@@ -6,11 +6,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>CollegeMapper  |  Stats</title>
 	{{ HTML::style('css/bootstrap.min.css')}}
+	{{HTML::style('css/stats.css')}}
 </head>
 <body>
 <div class="container">
 <div class="row">
 <div class="col-md-12">
+
 <div class="table-responsive">
 	<table class="table table-bordered table-condensed table-striped tablesorter" id="table">
 		<thead>
@@ -38,7 +40,7 @@
 					<td>{{$row['school']}}</td>
 					<td>{{$row['major']}}</td>				
 				@endif
-				<td>{{$row['milesfromhome']}}</td>
+				<td><span class="dist">{{$row['milesfromhome']}}</span> Miles</td>
 			</tr>
 		@endforeach
 		</tbody>
@@ -47,5 +49,27 @@
 </div>
 </div>
 </div>
+{{HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js')}}
+{{HTML::script('js/tablesorter.min.js')}}
+{{HTML::script('js/tablesorter.widgets.min.js')}}
+<script>
+$(document).ready(function(){ 
+	$("#table").tablesorter({
+		theme: 'bootstrap',
+		headerTemplate : '{content}{icon}',
+		widgets: ['uitheme', 'filter'],
+		sortList: [[0,0]],		
+		textExtraction: function(node) { 
+			var $n = $(node), $p = $n.find('.dist');
+			return $p.length ? $p.text() : $n.text(); 
+		},
+		emptyTo: 'bottom',
+		widgetOptions : {
+			filter_ignoreCase : true,
+			filter_liveSearch : true,
+		}
+	}); 
+}); 
+</script>
 </body>
 </html>
